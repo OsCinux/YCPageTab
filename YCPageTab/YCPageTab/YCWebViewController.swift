086bc69462bb794d2425e199c91d9336f563f66b
yeechain <yeechain@163.com>
2017-09-15 10:31:57 +0800

    【Feature】创建WebView文件

---
+++ YCPageTab/YCPageTab/YCWebViewController.swift
@@ -0,0 +1,15 @@
+//
+//  YCWebViewController.swift
+//  YCPageTab
+//
+//  Created by ljc on 12/09/2017.
+//  Copyright © 2017 ljc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class YCWebViewController: UIViewController {
+    var webUrlString: String? = "";
+    
+}
